66e00165a94afb8025c3970736e1b7469e4e37a9
8bitgeek <mike@8bitgeek.net>
2021-06-24 02:46:23 -0400

    include cook makefile snippet

---
+++ _src.mk
@@ -0,0 +1,24 @@
+# ** FREEMODBUS
+# SRC_FREEMODBUS=${ENV_FREEMODBUS_ROOT}
+SRC_LIBMODBUS=${SRC_ROOT}/freemodbus
+
+INC += 	-I $(SRC_FREEMODBUS) 			\
+		-I $(SRC_FREEMODBUS)/include 	\
+		-I $(SRC_FREEMODBUS)/ascii 		\
+		-I $(SRC_FREEMODBUS)/rtu 		\
+		-I $(SRC_FREEMODBUS)/tcp
+
+INC += -I $(SRC_BSP)/modbus/port
+
+SRCS_CC += $(SRC_FREEMODBUS)/mb.c
+SRCS_CC += $(SRC_FREEMODBUS)/functions/mbfunccoils.c
+SRCS_CC += $(SRC_FREEMODBUS)/functions/mbfuncdiag.c
+SRCS_CC += $(SRC_FREEMODBUS)/functions/mbfuncdisc.c
+SRCS_CC += $(SRC_FREEMODBUS)/functions/mbfuncholding.c
+SRCS_CC += $(SRC_FREEMODBUS)/functions/mbfuncinput.c
+SRCS_CC += $(SRC_FREEMODBUS)/functions/mbfuncother.c
+SRCS_CC += $(SRC_FREEMODBUS)/functions/mbutils.c
+SRCS_CC += $(SRC_FREEMODBUS)/ascii/mbascii.c
+SRCS_CC += $(SRC_FREEMODBUS)/rtu/mbcrc.c
+SRCS_CC += $(SRC_FREEMODBUS)/rtu/mbrtu.c
+SRCS_CC += $(SRC_FREEMODBUS)/tcp/mbtcp.c
